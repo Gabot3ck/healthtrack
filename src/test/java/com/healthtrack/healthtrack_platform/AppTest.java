d62dcab6cb96e00d1b9841a77ac5f1848e1b1a4b
Gabot3ck <jesus.castilloc@outlook.com>
2025-06-29 12:34:40 -0400

    se modifica el AppTest y se verifican funcionamiento de dependencias de test

--- src/test/java/com/healthtrack/healthtrack_platform/AppTest.java
+++ src/test/java/com/healthtrack/healthtrack_platform/AppTest.java
@@ -1,38 +1,25 @@
 package com.healthtrack.healthtrack_platform;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple App using JUnit 5
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    @DisplayName("Test básico de la aplicación")
+    public void testApp() {
+        assertTrue(true, "Test básico debe pasar");
     }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    
+    @Test
+    @DisplayName("Test de ejemplo con assertion")
+    public void testExample() {
+        String expected = "HealthTrack";
+        String actual = "HealthTrack";
+        assertEquals(expected, actual, "Los strings deben ser iguales");
     }
 }
